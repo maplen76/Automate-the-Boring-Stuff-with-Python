756a6dc0fb3bed1de327d32fe0275826fdeb2091
Alex <jingwang512@gmail.com>
2018-02-05 15:18:44 +0800

    Update ch5_dictionary.py

--- ch5_dictionary.py
+++ ch5_dictionary.py
@@ -14,3 +14,20 @@ def displayInventory(inventory):
         print(str(numberItem) + '    ' + k)
         totalItem = totalItem + numberItem
     print('Total number of items: ' + str(totalItem))
+    
+# List to Dictionary Function for fantasy game inventory
+def addToInventory(inventory, addedItems):
+    for i in addedItems:
+        if i in inventory.keys():
+            inventory[i] = 1 + inventory.get(i,0)
+        else:
+            inventory.setdefault(i, 1)
+    return inventory
+
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = {'gold coin': 42, 'rope': 1}
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
+
+    
