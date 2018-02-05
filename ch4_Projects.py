da2023a1ca853c98aa08691efe3d493a047fc925
Alex <jingwang512@gmail.com>
2018-02-05 10:20:35 +0800

    Update ch4_Projects.py

--- ch4_Projects.py
+++ ch4_Projects.py
@@ -9,3 +9,29 @@ def commaCode(spam):
     return a
 
 spam = ['apples', 'bananas', 'tofu', 'cats']
+
+
+
+# Character Picture Grid
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+def paste0(spam):
+    a = ''
+    for i in range(len(spam)):
+        a = a + str(spam[i])
+    return a
+
+def gridP(grid):    
+    for n in range(6):
+        a = ''
+        for i in range(len(grid)):
+            a = a + grid[i][n]
+        print(a)
